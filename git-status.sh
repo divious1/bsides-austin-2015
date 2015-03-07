54b91ca0c1e1d0d2a194310365ddae854ba4f596
divious1 <josehelps@gmail.com>
2015-03-07 18:11:02 +0000

    added cron job to check git status

---
+++ git-status.sh
@@ -0,0 +1,5 @@
+#script to write git status to logs
+cd /root/bsides-austin-2015/ 
+date >> /var/log/git-status.log 
+echo '-----------------' >> /var/log/git-status.log
+git status >> /var/log/git-status.log
